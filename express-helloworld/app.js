e4470e983d1e2144344968fdeab05b3cb30dbd4c
riddhishetty39 <110085479+riddhishetty39@users.noreply.github.com>
2022-08-25 17:24:50 +0530

    Update app.js

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Made changes now\n '+ new Date());
+  res.send('Changes updated\n '+ new Date());
 });
 
 app.listen(8080, function () {
